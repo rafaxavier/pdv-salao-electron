b0cc2eb25c4fce4d21941f3b328d1bd166732e97
rafaxavier <rxn.90s@gmail.com>
2024-04-05 00:20:00 -0300

    feat: search de clientes

--- src/pages/clientes.js
+++ src/pages/clientes.js
@@ -10,9 +10,9 @@ const getAllClientes = () => {
   });
 };
 
-const createCliente = (nome, telefone) => {
+const createCliente = (nome, telefone, cpf) => {
   return new Promise((resolve, reject) => {
-    window.electron.ipcRenderer.sendMessage('criarClientes', { nome, telefone });
+    window.electron.ipcRenderer.sendMessage('criarClientes', { nome, telefone, cpf });
     // Adicione um listener para receber os clientes
     window.electron.ipcRenderer.once('criarClientesResult', (resposta) => {
       console.log(resposta);
@@ -32,9 +32,9 @@ const deleteCliente = (id) => {
   });
 };
 
-const updateCliente = (id, nome, telefone) => {
+const updateCliente = (id, nome, telefone, cpf) => {
   return new Promise((resolve, reject) => {
-    window.electron.ipcRenderer.sendMessage('editarClientes', { id, nome, telefone });
+    window.electron.ipcRenderer.sendMessage('editarClientes', { id, nome, telefone, cpf });
     // Adicione um listener para receber os clientes
     window.electron.ipcRenderer.once('editarClientesResult', (resposta) => {
       console.log(resposta);
@@ -44,19 +44,19 @@ const updateCliente = (id, nome, telefone) => {
 };
 
 /***************************************************************************************** */
-// salvar cliente
-async function salvarCliente() {
-  const nome = document.getElementById('nome-cliente');
-  const telefone = document.getElementById('telefone-cliente');
+let todosClientes = [];
+const searchInput = document.getElementById('searchInput');
 
-  if (nome.value !== '' && telefone.value !== '' && telefone.value.length >= 14) {
-    await createCliente(nome.value, telefone.value);
-    nome.value = '';
-    telefone.value = '';
-    atualizarTabelaClientes();
-  }
-}
+searchInput.addEventListener('input', function () {
+  const searchText = this.value.toLowerCase();
+  const arr = todosClientes.filter(e => e.nome.toLowerCase().includes(searchText))
+  table.innerHTML = '';
+  criarListaDeClientes(arr)
+  console.log(arr)
 
+});
+
+// ####### mascaras para inputs
 function handlePhone(event) {
   let input = event.target
   input.value = phoneMask(input.value)
@@ -70,27 +70,68 @@ function phoneMask(value) {
   return value
 }
 
-// atualiza cliente
+function handleCpf(event) {
+  let input = event.target;
+  input.value = cpfMask(input.value);
+}
+
+function cpfMask(value) {
+  if (!value) return "";
+  value = value.replace(/\D/g, '');
+  value = value.replace(/(\d{3})(\d)/, "$1.$2");
+  value = value.replace(/(\d{3})(\d)/, "$1.$2");
+  value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  return value;
+}
+
+// ####### salvar cliente
+const closeModalButton = document.getElementById('close-modal-cria-cliente');
+closeModalButton.addEventListener('click', function() {
+    document.getElementById('modal-criar-cliente').style.display = 'none';
+});
+
+const openModalButton = document.getElementById('open-modal-cria-cliente');
+openModalButton.addEventListener('click', function() {
+    document.getElementById('modal-criar-cliente').style.display = 'block';
+});
+
+async function salvarCliente() {
+  const nome = document.getElementById('nome-cliente');
+  const telefone = document.getElementById('telefone-cliente');
+  const cpf = document.getElementById('cpf-cliente');
+
+  if (nome.value !== '' && telefone.value !== '' && telefone.value.length >= 14 && cpf.value.length == 14) {
+    await createCliente(nome.value, telefone.value, cpf.value);
+    nome.value = '';
+    telefone.value = '';
+    cpf.value = '';
+    document.getElementById('modal-criar-cliente').style.display = 'none';
+    atualizarTabelaClientes();
+  }
+}
+
+// ####### atualiza cliente
 async function atualizaCliente() {
   const id = document.getElementById('id-cliente').value;
   const nome = document.getElementById('edit-nome').value;
   const telefone = document.getElementById('edit-telefone').value;
+  const cpf = document.getElementById('edit-cpf').value;
 
-  if (nome !== '' && telefone !== '' && telefone.length >= 14) {
-    await updateCliente(id, nome, telefone);
+  if (nome !== '' && telefone !== '' && telefone.length >= 14 && cpf.length == 14) {
+    await updateCliente(id, nome, telefone, cpf);
     document.getElementById('modal-editar-cliente').style.display = "none";
     atualizarTabelaClientes();
   }
 }
 
-// deleta cliente
+// ####### deleta cliente
 async function deletaCliente(id) {
   await deleteCliente(id);
   atualizarTabelaClientes();
 }
 
 // modal edite cliente
-async function renderModalEditCliente(id, nome, telefone) {
+async function renderModalEditCliente(id, nome, telefone, cpf) {
   const modalEditar = document.getElementById('modal-editar-cliente');
   const span = document.getElementsByClassName("close")[0];
   modalEditar.style.display = "block";
@@ -98,6 +139,7 @@ async function renderModalEditCliente(id, nome, telefone) {
   document.getElementById('id-cliente').value = id;
   document.getElementById('edit-nome').value = nome;
   document.getElementById('edit-telefone').value = telefone;
+  document.getElementById('edit-cpf').value = cpf;
 
   span.addEventListener('click', () => {
     modalEditar.style.display = "none";
@@ -115,15 +157,18 @@ function criarListaDeClientes(clientes) {
   const th1 = document.createElement('th');
   const th2 = document.createElement('th');
   const th3 = document.createElement('th');
+  const th4 = document.createElement('th');
 
   th0.textContent = 'ID';
   th1.textContent = 'Nome';
   th2.textContent = 'Telefone';
-  th3.textContent = 'Ação';
+  th3.textContent = 'Cpf';
+  th4.textContent = 'Ação';
   headerRow.appendChild(th0);
   headerRow.appendChild(th1);
   headerRow.appendChild(th2);
   headerRow.appendChild(th3);
+  headerRow.appendChild(th4);
   table.appendChild(headerRow);
 
   // Adiciona cada cliente como uma linha na tabela
@@ -133,12 +178,14 @@ function criarListaDeClientes(clientes) {
     const column1 = document.createElement('td');
     const column2 = document.createElement('td');
     const column3 = document.createElement('td');
+    const column4 = document.createElement('td');
     const deleteButton = document.createElement('button');
     const editButton = document.createElement('button');
 
     column0.textContent = cliente.id;
     column1.textContent = cliente.nome;
     column2.textContent = cliente.telefone;
+    column3.textContent = cliente.cpf;
 
     editButton.textContent = 'Editar';
     editButton.classList.add('btn-warning', 'btn-sm');
@@ -147,15 +194,16 @@ function criarListaDeClientes(clientes) {
 
     deleteButton.addEventListener('click', () => deletaCliente(cliente.id));
 
-    editButton.addEventListener('click', () => renderModalEditCliente(cliente.id, cliente.nome, cliente.telefone));
+    editButton.addEventListener('click', () => renderModalEditCliente(cliente.id, cliente.nome, cliente.telefone, cliente.cpf));
 
-    column3.appendChild(editButton);
-    column3.appendChild(deleteButton);
+    column4.appendChild(editButton);
+    column4.appendChild(deleteButton);
 
     row.appendChild(column0);
     row.appendChild(column1);
     row.appendChild(column2);
     row.appendChild(column3);
+    row.appendChild(column4);
 
     // Adiciona a linha à tabela
     table.appendChild(row);
@@ -169,12 +217,13 @@ function criarListaDeClientes(clientes) {
 /**-------------------------------------------------------------------------------------- */
 function atualizarTabelaClientes() {
   // Limpa o conteúdo da tabela
-  table.innerHTML = ''; // Limpa o conteúdo da tabela
-
+  searchInput.value = ''; // Limpa o conteúdo da tabela
+  table.innerHTML = '';
   // Obtém a lista atualizada de clientes e recria a tabela
   getAllClientes()
     .then(clientes => {
-      criarListaDeClientes(clientes);
+      todosClientes = clientes
+      criarListaDeClientes(todosClientes);
     })
     .catch(error => {
       console.error('Erro ao carregar clientes:', error);
@@ -185,7 +234,8 @@ function atualizarTabelaClientes() {
 window.addEventListener('DOMContentLoaded', () => {
 
   getAllClientes().then(clientes => {
-    criarListaDeClientes(clientes);
+    todosClientes = clientes
+    criarListaDeClientes(todosClientes);
   }).catch(error => {
     console.error('Erro ao carregar clientes:', error);
   });
