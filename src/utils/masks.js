da9163b6fcd2494e5737ea3a1649325054c0631c
rafaxavier <rxn.90s@gmail.com>
2024-06-23 21:48:50 -0300

    feat: funcao para formatacao de data

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -52,3 +52,14 @@ export function unmaskPercent(value) {
   value = parseFloat(value) / 100;
   return value;
 }
+
+export function formatDateTime(dateTimeString) {
+  const date = new Date(dateTimeString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Mês começa do zero
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
