3309ac081a92fbd744984c2b395d29c1a5afb641
rafaxavier <rxn.90s@gmail.com>
2024-07-05 01:29:43 -0300

    feat: add funcao de criacao de pdf

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -1,3 +1,5 @@
+import { getOneColaboradorByName } from '../requests/colaboradores-ipc.js';
+
 export function phoneMask(value) {
   if (!value) return ""
   value = value.replace(/\D/g, '')
@@ -7,12 +9,12 @@ export function phoneMask(value) {
 }
 
 export function cpfMask(value) {
-    if (!value) return "";
-    value = value.replace(/\D/g, '');
-    value = value.replace(/(\d{3})(\d)/, "$1.$2");
-    value = value.replace(/(\d{3})(\d)/, "$1.$2");
-    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
-    return value;
+  if (!value) return "";
+  value = value.replace(/\D/g, '');
+  value = value.replace(/(\d{3})(\d)/, "$1.$2");
+  value = value.replace(/(\d{3})(\d)/, "$1.$2");
+  value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  return value;
 }
 
 //  **** Mascara de monetária
@@ -72,15 +74,187 @@ export function convertWeekToDatetimeLocal(weekString) {
   const startDate = new Date(firstDayOfYear.getTime() + (week - 1) * 7 * 24 * 60 * 60 * 1000 - daysOffset * 24 * 60 * 60 * 1000);
   const endDate = new Date(startDate.getTime() + 6 * 24 * 60 * 60 * 1000);
 
-  const formatDateTimeLocal = (date) => {
+  const formatDateTimeLocal = (date, isEndOfDay = false) => {
     const yearStr = date.getUTCFullYear();
     const monthStr = String(date.getUTCMonth() + 1).padStart(2, '0');
     const dayStr = String(date.getUTCDate()).padStart(2, '0');
-    return `${yearStr}-${monthStr}-${dayStr}T00:00`;
+    const timeStr = isEndOfDay ? '23:59' : '00:00';
+    return `${yearStr}-${monthStr}-${dayStr}T${timeStr}`;
   };
 
   return {
     start: formatDateTimeLocal(startDate),
-    end: formatDateTimeLocal(endDate)
+    end: formatDateTimeLocal(endDate, true)
   };
+}
+
+export function convertMonthToDatetimeLocal(monthString) {
+  const [year, month] = monthString.split('-').map(Number);
+
+  const startDate = new Date(Date.UTC(year, month - 1, 1));
+  const endDate = new Date(Date.UTC(year, month, 0)); // Último dia do mês
+
+  const formatDateTimeLocal = (date, isEndOfDay = false) => {
+    const yearStr = date.getUTCFullYear();
+    const monthStr = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const dayStr = String(date.getUTCDate()).padStart(2, '0');
+    const timeStr = isEndOfDay ? '23:59' : '00:00';
+    return `${yearStr}-${monthStr}-${dayStr}T${timeStr}`;
+  };
+
+  return {
+    start: formatDateTimeLocal(startDate),
+    end: formatDateTimeLocal(endDate, true) // Configura o final do dia como 23:59
+  };
+}
+
+export async function generatePDF(arr, periodo) {
+  const allServices = arr.flatMap(e => e.servicos.map(servico => ({
+    idVenda: e.id,
+    cliente: e.cliente,
+    colaborador: e.colaborador,
+    data: formatDateTime(e.data),
+    ...servico
+  })));
+
+  const colaborador = await getOneColaboradorByName(allServices[0].colaborador);
+  const dtPeriodo = periodo ?? `Período: ${allServices[0].data} á ${allServices[allServices.length - 1].data}`;
+
+  const totals = allServices.reduce((acc, service) => {
+    acc.preco += service.preco;
+    acc.taxa += service.taxa;
+    acc.desconto += service.desconto;
+    acc.repasse += service.repasse;
+    return acc;
+  }, { preco: 0, taxa: 0, desconto: 0, repasse: 0 });
+
+  let props = {
+    outputType: jsPDFInvoiceTemplate.OutputType.Save,
+    returnJsPDFDocObject: true,
+    fileName: "Relatório 2021",
+    orientationLandscape: false,
+    compress: true,
+    logo: {
+      // src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
+      src: "../../../assets/logo.png",
+      type: 'PNG', //optional, when src= data:uri (nodejs case)
+      width: 35, //aspect ratio = width/height
+      height: 35,
+      margin: {
+        top: -5, //negative or positive num, from the current position
+        left: -5 //negative or positive num, from the current position
+      }
+    },
+    business: {
+      name: "Espaço Raynne Souza",
+      address: "Tv. Um, 137 - Recreio, Rio das Ostras - RJ",
+      phone: "(+22) 99999-9999",
+      email: "email@example.com",
+      website: "www.example.com",
+    },
+    contact: {
+      label: `${colaborador.res.profissao}: `,
+      name: colaborador.res.nome,
+      address: `CPF: ${colaborador.res.cpf}`,
+      // phone: `CPF: ${colaborador.res.cpf}`,
+      // email: `CPF: ${colaborador.res.cpf}`,
+    },
+    invoice: {
+      label: dtPeriodo,
+      num: '.',
+      // invDate: dtPeriodo,
+      // invGenDate: "Período: 02/02/2021 10:17",
+      headerBorder: true,
+      tableBodyBorder: true,
+      header: [
+        {
+          title: "COD",
+          style: {
+            width: 10
+          }
+        },
+        {
+          title: "Cliente",
+          style: {
+            width: 40
+          }
+        },
+        {
+          title: "Serviços",
+          style: {
+            width: 45
+          }
+        },
+        {
+          title: "Horário",
+          style: {
+            width: 30,
+          }
+        },
+        {
+          title: "Preço",
+          style: {
+            width: 20,
+          }
+        },
+        {
+          title: "Taxa",
+          style: {
+            width: 12,
+          }
+        },
+        {
+          title: "Desconto",
+          style: {
+            width: 18,
+          }
+        },
+        {
+          title: "Repasse",
+          style: {
+            width: 18,
+          }
+        }
+      ],
+      table: allServices.map(service => [
+        service.idVenda,
+        service.cliente,
+        service.nome,
+        service.data,
+        service.preco.toFixed(2),
+        (service.taxa * 100).toFixed(0) + '%',
+        service.desconto.toFixed(2),
+        service.repasse.toFixed(2)
+      ]),
+      additionalRows: [{
+        col1: 'Total:',
+        col2: totals.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
+        style: {
+          fontSize: 10 //optional, default 12
+        }
+      },
+      {
+        col1: 'Desconto:',
+        col2: '- ' + totals.desconto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
+        style: {
+          fontSize: 10 //optional, default 12
+        }
+      },
+      {
+        col1: 'SubTotal(Repasse):',
+        col2: totals.repasse.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
+        style: {
+          fontSize: 14 //optional, default 12
+        }
+      }],
+    },
+    footer: {
+      text: "The invoice is created on a computer and is valid without the signature and stamp.",
+    },
+    pageEnable: true,
+    pageLabel: "Page ",
+
+  };
+
+  jsPDFInvoiceTemplate.default(props);
 }
