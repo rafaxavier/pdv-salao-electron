e7fd44549fc3a040fc4dd2663669e3ff427ec140
rafaxavier <rxn.90s@gmail.com>
2024-07-06 03:03:16 -0300

    refactor: passando funcao para o arquivo masks

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -258,3 +258,19 @@ export async function generatePDF(arr, periodo) {
 
   jsPDFInvoiceTemplate.default(props);
 }
+
+export function getTimeNow() {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let localDatetime = year + "-" +
+    (month < 10 ? "0" + month.toString() : month) + "-" +
+    (day < 10 ? "0" + day.toString() : day) + "T" +
+    (hour < 10 ? "0" + hour.toString() : hour) + ":" +
+    (minute < 10 ? "0" + minute.toString() : minute);
+
+  return localDatetime;
+}
